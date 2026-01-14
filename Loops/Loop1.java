5856079e3b3548ce5ff17fd9e48c1cbe65f4457a
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-01-14 20:07:02 +0530

    Create Loop1.java

---
+++ Loops/Loop1.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        //WAP TO PRINT 1 TO 10 NATURAL NUMBERS ?
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
