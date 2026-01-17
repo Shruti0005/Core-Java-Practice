250e8469d11729719580dee218f968dc8114e7b2
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-01-18 00:48:56 +0530

    Update Star8.java
    Changes in program file

--- Patterns/Star8.java
+++ Patterns/Star8.java
@@ -21,15 +21,16 @@ public class Star8 {
         }
 
         //Lowwer part of the diamond
-        for (int i = 1; i <= star-1; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = star-1; i >= 1; i--) {
+            for (int j = star-1; j >= i; j--) {
                 System.out.print("  ");
             }
-           for (int j = star*2-i-1; j >= i+1; j--) {
-            System.out.print("* ");
-           }
+            for (int j = 1; j <= i*2-1; j++) {
+                System.out.print("* ");
+            }
             System.out.println();
         }
         sc.close();
     }
 }
+
