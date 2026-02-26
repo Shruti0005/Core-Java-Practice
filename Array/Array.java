9aa66f422c092c1c5b08fcf049c7afcea479a543
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-02-26 19:08:00 +0530

    Update Array.java

--- Array/Array.java
+++ Array/Array.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Array10 {
     public static void main(String[] args) {
-        //Print element vertically
+        //Store elements in seperate array
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of ");
         int length = sc.nextInt();
@@ -13,9 +13,15 @@ public class Array10 {
             System.out.println("Enter the element "+i+": ");
             arr[i] = sc.nextInt();
         }
+        int arr2[] = new int[arr.length];
+        int j = 0;
         for (int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+            arr2[i] = arr[j++];
         }
+
+        for(int i : arr2){
+            System.out.print(i + " ");
+        }    
         sc.close();
     }
 }
