288d916871ca8cbfa60b6301058a0ec160ebb2b3
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-04-09 16:30:37 +0530

    Add files via upload

---
+++ If_Else/Swap.java
@@ -0,0 +1,26 @@
+package Conditional_Statements;
+
+import java.util.Scanner;
+
+public class Swap {
+	public static void main(String[] args) {
+    //WAP TO SWAP TWO VARIABLE WITH USING THIRD VARIABLE?
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first value: ");
+		int num1 = sc.nextInt();
+		System.out.print("\nEnter the second value: ");
+		int num2 = sc.nextInt();
+		
+		if (num1 != num2) {
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;			
+			System.out.println("First value: " + num1 + "\nSecond value: " + num2);
+		}
+		else {
+			System.out.println("Sorry! we can't swap because both value are same.");
+		}
+		
+		sc.close();
+	}
+}
---
+++ If_Else/SwapWithoutThirdNumber3.java
@@ -0,0 +1,26 @@
+package Conditional_Statements;
+
+import java.util.Scanner;
+
+public class SwapWithoutThirdNumber3 {
+	public static void main(String[] args) {
+		//WAP TO SWAP TWO VARIABLE WITHOUT USING THIRD VARIABLE?
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first value: ");
+		int num1 = sc.nextInt();
+		System.out.print("\nEnter the second value: ");
+		int num2 = sc.nextInt();
+		
+		if (num1 != num2) {
+			num1 = num1 + num2;
+			num2 = num1 - num2;
+			num1 = num1 - num2;
+			System.out.println("First value: " + num1 + "\nSecond value: " + num2);
+		}
+		else {
+			System.out.println("Sorry! we can't swap because both value are same.");
+		}
+		
+		sc.close();
+	}
+}
