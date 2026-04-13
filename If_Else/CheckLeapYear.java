278f1c0b1f86c77dbec1f24569d00cb2e1d7bac2
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-04-13 11:57:08 +0530

    Update CheckLeapYear.java

--- If_Else/CheckLeapYear.java
+++ If_Else/CheckLeapYear.java
@@ -3,18 +3,20 @@ package If_Else;
 import java.util.Scanner;
 
 public class CheckLeapYear {
-    public static void main(String[] args) {
-        //Check whether leap year or not 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the year in this form => XXXX ");
-        int year = sc.nextInt();
+	public static void main(String[] args) {
+		//WAP to check whether a year is a leap year or not
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the year in this form(xxxx): ");
+		int num = sc.nextInt();
+		
+		if ((num % 4 == 0 && num % 100 != 0) || (num % 400 == 0)) {
+			System.out.println(num + " is a leap year.");
+		}
+		else { 
+			System.out.println(num + " is not a leap year.");
+		}
+		sc.close();
+	}
 
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " is a leap year");
-        }
-        else{
-            System.out.println(year +" is not a leap year");
-        }
-        sc.close();
-    }
 }
+
