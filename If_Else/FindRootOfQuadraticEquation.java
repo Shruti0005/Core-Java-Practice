22114acba9ef6e164510ef02e5a075a8ab22ad5a
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-04-27 17:00:11 +0530

    Update FindRootOfQuadraticEquation.java

--- If_Else/FindRootOfQuadraticEquation.java
+++ If_Else/FindRootOfQuadraticEquation.java
@@ -6,17 +6,28 @@ public class FindRootOfQuadraticEquation {
 	public static void main(String[] args) {
 		//WAP to find roots of quadratic equation -> condition: discriminant > 0 → real & different = 0 → real & same < 0 → imaginary
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the discriminant value: ");
-		int d = sc.nextInt();
+		System.out.print("Enter the value of a: ");
+		int a = sc.nextInt();
+		System.out.print("Enter the value of b: ");
+		int b = sc.nextInt();
+		System.out.print("Enter the value of c: ");
+		int c = sc.nextInt();
 		
-		if(d > 0) {
-			System.out.println(d + " Two real and different roots.");
+		if(a == 0) {
+			System.out.println("Not qudratic equation.");
 		}
-		else if(d == 0) {
-			System.out.println(d + " Real and equal roots");
-		}
-		else if(d < 0) {
-			System.out.println(d + " Imaginary roots");
+		else {
+			int d = (b * b) - (4 * a * c);
+			
+			if(d > 0) {
+				System.out.println(d + " Two real and different roots.");
+			}
+			else if(d == 0) {
+				System.out.println(d + " Real and equal roots");
+			}
+			else{
+				System.out.println(d + " Imaginary roots");
+			}
 		}
 		
 		sc.close();
